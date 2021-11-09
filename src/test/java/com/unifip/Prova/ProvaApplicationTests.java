2cde840f941220c5b19c63726b015cd4ca71cbfc
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-11-09 18:34:32 -0300

    initialize project

---
+++ src/test/java/com/unifip/Prova/ProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifip.Prova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
