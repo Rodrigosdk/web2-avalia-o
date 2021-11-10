d1ee6d151ed6269bd85e3bd8e6884159f7c7d3b4
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-11-09 22:45:35 -0300

    refactod controller

--- src/main/java/com/unifip/Prova/controller/ContaController.java
+++ src/main/java/com/unifip/Prova/controller/ContaController.java
@@ -1,10 +1,10 @@
 package com.unifip.Prova.controller;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.unifip.Prova.model.Conta;
-import com.unifip.Prova.model.Status;
+import com.unifip.Prova.services.ContaServices;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,43 +17,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/contas")
 public class ContaController {
-    private Map<Long, Conta> contaDB = new HashMap<>();
+    private ContaServices contaServices = new ContaServices();
 
     @ResponseStatus(code = HttpStatus.CREATED)
     @RequestMapping(value = "", method = RequestMethod.POST)
     public void cadastrarConta(@RequestBody Conta conta) {
-        conta.setStatus(Status.PENDENTE);
-        this.contaDB.put(conta.getId(), conta);
+        contaServices.cadastrarConta(conta);
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public Map<Long, Conta> listarContas() {
-        return contaDB;
+        return contaServices.listarContas();
     }
 
     @RequestMapping(value = "/pagar", method = RequestMethod.PUT)
     public void pagarConta(@RequestParam Long id) {
-        Conta conta = this.contaDB.get(id);
-        conta.setStatus(Status.PAGO);
-        this.contaDB.put(id, conta);
+        contaServices.pagarConta(id);
     }
 
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @RequestMapping(value = "", method = RequestMethod.DELETE)
     public void cancelarConta(@RequestParam Long id) {
-        this.contaDB.remove(id);
+        contaServices.cancelarConta(id);
     }
 
     @RequestMapping(value = "/buscar", method = RequestMethod.GET)
     public Conta buscarContaID(@RequestParam Long id) {
-        return this.contaDB.get(id);
+       return contaServices.buscarContaID(id);
     }
-    /**
-     * 
-     * 
-     * 
-     * @RequestMapping(value = "", method = RequestMethod.GET) public void
-     *                       contasStatus() { }
-     */
 
+    @RequestMapping(value = "/contasStatus", method = RequestMethod.GET)
+    public List<String>  contasStatus() {
+        return contaServices.contasStatus();
+    }
 }
